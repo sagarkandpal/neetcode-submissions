003543daad1ad869b6d9850ff010225443ea99e7
Sagar kandpal <149861518+sagarkandpal@users.noreply.github.com>
2026-07-03 19:57:44 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length() != t.length()){
+            return false;
+        }
+
+
+        int counts[] = new int[26];
+
+        for(int i=0; i<s.length(); i++){
+            counts[s.charAt(i) - 'a']++;
+        }
+
+        for(int j=0; j<t.length(); j++){
+            counts[t.charAt(j) - 'a']--;
+        }
+
+        for(int count: counts){
+            if(count != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+ 
